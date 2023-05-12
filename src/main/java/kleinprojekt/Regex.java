a3c545244e5190c4f303953828d855db79481bed
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2023-05-12 11:47:01 +0200

    delete test Regex

--- src/main/java/kleinprojekt/Regex.java
+++ src/main/java/kleinprojekt/Regex.java
@@ -2,15 +2,10 @@ package kleinprojekt;
 
 public class Regex {
 	
-	private String regex0 = "^.[^\\s]{2,10}$";
 	private String regex1 = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%|*?&+-])[A-Za-z\\d@$!%|*?&+-]{8,}$";
 	private String regex2 = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%|*?&+-])[A-Za-z\\d@$!%|*?&+-]{8,}$";
 	private String regex3 = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%|*?&+-])[A-Za-z\\d@$!%|*?&+-]{12,}$";
 	private String regex4 = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%|*?&+-])[A-Za-z\\d@$!%|*?&+-]{16,}$";
-	
-	public String getRegex0() {
-		return regex0;
-	}
 	
 	public String getRegex1() {
 		return regex1;
